233fa16085d6267be75619649478e0a8355c7a94
dbfjru <dbfjru@gmail.com>
2024-03-20 11:17:12 +0900

    🔧 chore: 코드 정리

--- src/main/java/io/superson/trelloproject/domain/user/controller/UserController.java
+++ src/main/java/io/superson/trelloproject/domain/user/controller/UserController.java
@@ -12,7 +12,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +28,8 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<ResponseDto<Void>> signUp(
-        @RequestBody @Validated SignUpRequestDto requestDto) {
+        @RequestBody @Validated SignUpRequestDto requestDto
+    ) {
         userService.signUp(requestDto);
 
         return ResponseEntity.ok().build();
@@ -32,14 +38,17 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<ResponseDto<Void>> login(
         @RequestBody @Validated LoginRequestDto requestDto,
-        HttpServletResponse httpServletResponse) {
+        HttpServletResponse httpServletResponse
+    ) {
         userService.login(requestDto, httpServletResponse);
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<ResponseDto<UserResponseDto>> getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<ResponseDto<UserResponseDto>> getUserInfo(
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         UserResponseDto userResponseDto = userService.getUserInfo(
             userDetails.getUser().getUserId());
         return ResponseEntity.ok().body(ResponseDto.<UserResponseDto>builder()
@@ -50,21 +59,24 @@ public class UserController {
     @PatchMapping
     public ResponseEntity<ResponseDto<Void>> updatePassword(
         @RequestBody PasswordUpdateRequestDto requestDto,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         userService.passwordUpdate(requestDto, userDetails.getUser());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
     public ResponseEntity<ResponseDto<Void>> logout(
-        HttpServletResponse httpServletResponse) {
+        HttpServletResponse httpServletResponse
+    ) {
         userService.logout(httpServletResponse);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/withdraw")
     public ResponseEntity<ResponseDto<Void>> withdraw(
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         userService.withdraw(userDetails.getUser().getUserId());
         return ResponseEntity.ok().build();
     }
